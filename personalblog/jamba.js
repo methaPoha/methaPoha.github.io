1f6f1be15a38fe431030cbfce043b6c58b2fb8ef
sen <priyanshu@sen.in>
2024-12-27 13:32:02 +0530

    feb 14

--- personalblog/jamba.js
+++ personalblog/jamba.js
@@ -2,7 +2,7 @@
 document.addEventListener('DOMContentLoaded', function() {
     // List of blogs with date (YYYY-MM-DD format recommended)
     const blogs = [
-        { folder: 'piece', date: '2024-12-25' },
+        { folder: 'piece', date: '2024-12-27' },
         { folder: 'day1', date: '2024-12-23' }
     ];
 
