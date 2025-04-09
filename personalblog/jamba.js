9616823c24591dc8acf3cee6370059f551ee3ac9
international girl <international@trump.supporter>
2025-04-09 17:20:34 +0530

    thik hu bhai, zinda hu

--- personalblog/jamba.js
+++ personalblog/jamba.js
@@ -2,9 +2,9 @@
 document.addEventListener('DOMContentLoaded', function() {
     // List of blogs with date (YYYY-MM-DD format recommended)
     const blogs = [
-        { folder: 'piece', date: '2025-03-23' },
+        { folder: 'piece', date: '2025-04-06' },
         { folder: 'day1', date: '2024-12-23' },
-        {folder: 'personalthought', date: '2025-03-23'},
+        {folder: 'personalthought', date: '2025-04-09'},
         {folder: 'S17', date: '2025-02-02'},
         {folder: 'dismokes', date: '2025-02-10'},
         {folder: 'GRdata', date: '2025-02-26'},
