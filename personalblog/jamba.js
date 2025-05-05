d754d08f4d50277f6778c162744400178d62970a
killing myself <kms@stab.in>
2025-05-05 23:21:45 +0530

    updates

--- personalblog/jamba.js
+++ personalblog/jamba.js
@@ -4,7 +4,7 @@ document.addEventListener('DOMContentLoaded', function() {
     const blogs = [
         { folder: 'piece', date: '2025-05-02' },
         { folder: 'day1', date: '2024-12-23' },
-        {folder: 'personalthought', date: '2025-05-02'},
+        {folder: 'personalthought', date: '2025-05-05'},
         {folder: 'S17', date: '2025-02-02'},
         {folder: 'dismokes', date: '2025-04-17'},
         {folder: 'GRdata', date: '2025-02-26'},
