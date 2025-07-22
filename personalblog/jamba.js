b379ad197c419301b59dfd907c3fea06f5bc5fb7
ayeayetea alluminium <raghar@ayeayetea.kgp>
2025-07-22 20:29:12 +0530

    why am i even doing this

--- personalblog/jamba.js
+++ personalblog/jamba.js
@@ -4,7 +4,7 @@ document.addEventListener('DOMContentLoaded', function() {
     const blogs = [
         { folder: 'piece', date: '2025-06-26' },
         { folder: 'day1', date: '2024-12-23' },
-        {folder: 'personalthought', date: '2025-07-10'},
+        {folder: 'personalthought', date: '2025-07-22'},
         {folder: 'S17', date: '2025-07-02'},
         {folder: 'dismokes', date: '2025-04-17'},
         {folder: 'GRdata', date: '2025-02-26'},
